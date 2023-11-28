09cb9abdc2f191c5caf260eb15fa603531d55307
Kostas Kougios <kostas.kougios@googlemail.com>
2023-11-28 15:21:06 +0000

    -

---
+++ src/sessions/Sessions.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Sessions() {
+    return (
+        <div className='sessions'>
+            <li>
+                <ul>Session 1</ul>
+                <ul>Session 2</ul>
+            </li>
+        </div>
+    )
+}
+
+export default Sessions;
