91774556dd97006c6e4b5fd1674cb9215349f79d
Kostas Kougios <kostas.kougios@googlemail.com>
2023-12-04 12:57:10 +0000

    -

--- src/terminal/mapStd.tsx
+++ src/terminal/mapStd.tsx
@@ -5,7 +5,10 @@ export function mapStd(msg: any): JSX.Element | null {
         {msg.Std.elements.map((element: any, index: number) => {
           if (element.Paragraph) {
             return <p>{element.Paragraph.text}</p>;
+          } else   if (element.Header1) {
+            return <h1>{element.Header1.text}</h1>
           }
+          return null;
         })}
       </>
     );
