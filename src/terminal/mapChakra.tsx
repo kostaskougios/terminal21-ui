5027a181163f5894d899f39a0a55658604f9ff3e
Kostas Kougios <kostas.kougios@googlemail.com>
2023-12-05 15:07:20 +0000

    -

--- src/terminal/mapChakra.tsx
+++ src/terminal/mapChakra.tsx
@@ -1,27 +1,11 @@
 import { Button, Box } from "@chakra-ui/react";
+import { ComponentRenderFunction, renderIfExists } from "./renderElement";
 
 export function mapChakra(msg: any): JSX.Element | null {
-  if (msg.Chakra) {
-    const element = msg.Chakra.element;
-    return <>{renderElement(element)}</>;
-  }
-  return null;
+  return renderIfExists(ElementMap,msg.Chakra);
 }
 
-type ComponentRenderFunction = (props: any) => JSX.Element;
-const m: Record<string, ComponentRenderFunction> = {
+const ElementMap: Record<string, ComponentRenderFunction> = {
   Button: (b: any) => <Button key={b.key}>{b.text}</Button>,
   Box: (b: any) => <Box key={b.key}>{b.text}</Box>,
 };
-
-function renderElement(element: any) {
-  Object.keys(element).map((key) => {
-    if (m[key]) return m[key](element[key]);
-  });
-  const p = element.Button;
-  if (p) {
-    return <Button key={p.key}>{p.text}</Button>;
-  }
-
-  return null;
-}
--- src/terminal/mapStd.tsx
+++ src/terminal/mapStd.tsx
@@ -1,21 +1,10 @@
+import { ComponentRenderFunction, renderIfExists } from "./renderElement";
+
 export function mapStd(msg: any): JSX.Element | null {
-  if (msg.Std) {
-    const element = msg.Std.element;
-    return <>{renderElement(element)}</>;
-  }
-  return null;
+  return renderIfExists(ElementMap,msg.Std);
 }
 
-function renderElement(element: any) {
-  const p = element.Paragraph;
-  if (p) {
-    return <p key={p.key}>{p.text}</p>;
-  }
-
-  const h1 = element.Header1;
-  if (h1) {
-    return <h1 key={h1.key}>{h1.text}</h1>;
-  }
-
-  return null;
-}
+const ElementMap: Record<string, ComponentRenderFunction> = {
+  Paragraph: (b: any) => <p key={b.key}>{b.text}</p>,
+  Header1: (b: any) => <h1 key={b.key}>{b.text}</h1>,
+};
