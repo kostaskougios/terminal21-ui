a0352518a9ad7f365df20ebbe3da791dcc455631
Kostas Kougios <kostas.kougios@googlemail.com>
2023-12-17 19:56:48 +0000

    -

--- src/service/WebSocketService.ts
+++ src/service/WebSocketService.ts
@@ -1,34 +1,56 @@
 import WsRequest from "./json/WsRequest";
+import { EventEmitter } from 'events';
 
 type OnOpenHandler = () => void;
 type MessageHandler = (message: any) => void;
 
 class WebSocketService {
   private socket: WebSocket | null = null;
-  private responses: any[] = [];
   private messageHandlers: MessageHandler[] = [];
   private onOpenHandlers: OnOpenHandler[] = [];
+  private outbound: WsRequest[] = [];
+  private emitter = new EventEmitter();
 
   constructor(
     public name: string,
     private url: string,
-  ) {}
+  ) {
+    this.emitter.on('new-message', () => {
+      this.sendOutbound();
+    });
+  }
 
-  public connect(): void {
-    this.reconnect();
+  private sendOutbound(): void {
+    if (this.socket == null) throw `Error, somehow sendOutbound called with null socket`;
+    if (this.socket.readyState === WebSocket.OPEN) {
+      console.log(`Sending ${this.outbound.length}  outbound messages`);
+      for (var i = 0; i < this.outbound.length; i++) {
+        const message = this.outbound[i];
+        console.log(`${this.name}: Sending `, message);
+        this.socket.send(message.toJSON());
+      }
+      this.outbound = [];
+    } else {
+      console.log(`Can't send outbound messages because websocket not open, will try to reconnect.`);
+      this.reconnect();
+    }
+  }
 
-    this.socket!.onopen = () => {
-      // console.log(
-      //   `${this.name}: WebSocket connection established, executing ${this.onOpenHandlers.length} on-open-handlers`,
-      // );
+  private reconnect(): void {
+    console.log(`${this.name}: connecting to ${this.url}`);
+    this.socket = new WebSocket(this.url);
+    this.socket.onopen = () => {
+      console.log(
+        `${this.name}: WebSocket connection established, executing ${this.onOpenHandlers.length} on-open-handlers`,
+      );
       this.onOpenHandlers.forEach((handler) => handler());
+      this.sendOutbound();
     };
 
-    this.socket!.onmessage = (event) => {
+    this.socket.onmessage = (event) => {
       console.log(`${new Date()} ${this.name}: received:`, event.data);
       try {
         const message = JSON.parse(event.data);
-        this.responses.push(message);
         this.messageHandlers.forEach((handler) => handler(message));
       } catch (error) {
         console.log(
@@ -40,44 +62,32 @@ class WebSocketService {
       }
     };
 
-    this.socket!.onclose = () => {
+    this.socket.onclose = () => {
       console.log(`${this.name}: WebSocket connection closed`);
-      setTimeout(() => this.attemptReconnect(), 500);
+      setTimeout(() => this.reconnect(), 500);
+      this.socket = null;
     };
 
-    this.socket!.onerror = (error) => {
+    this.socket.onerror = (error) => {
       console.error(`${this.name}: WebSocket error:`, error);
     };
   }
 
-  private reconnect(): void {
-    console.log(`${this.name}: connecting to ${this.url}`);
-    this.socket = new WebSocket(this.url);
+  public connect(): void {
+    this.reconnect();
   }
 
   public disconnect(): void {
     console.log(`${this.name}: disconnect called.`);
     if (this.socket) {
       this.socket.close();
-    }
-  }
-  private attemptReconnect(): void {
-    if (!this.socket || this.socket.readyState === WebSocket.CLOSED) {
-      console.log("Attempting to reconnect...");
-      this.connect();
+      this.socket = null;
     }
   }
 
   public send(message: WsRequest): void {
-    if (this.socket && this.socket.readyState === WebSocket.OPEN) {
-      console.log(`${this.name}: Sending `, message);
-      this.socket.send(message.toJSON());
-      // console.log("Msg was send.");
-    } else {
-      throw `${this.name}: Message ${message.toJSON()} not send. Socket = ${
-        this.socket
-      }. readyState = ${this.socket?.readyState}`;
-    }
+    this.outbound.push(message);
+    this.emitter.emit('new-message');
   }
 
   public subscribeToMessages(handler: MessageHandler): void {
@@ -86,10 +96,6 @@ class WebSocketService {
 
   public subscribeToOnOpen(handler: OnOpenHandler): void {
     this.onOpenHandlers.push(handler);
-  }
-
-  public getResponses(): any[] {
-    return this.responses;
   }
 }
 
