0e3bd91624116719720068c5bb75387f47bb7952
Kostas Kougios <kostas.kougios@googlemail.com>
2024-01-02 17:17:58 +0000

    -

--- src/terminal/mapChakra.test.tsx
+++ src/terminal/mapChakra.test.tsx
@@ -1,10 +1,22 @@
 import { render, screen, waitFor } from "@testing-library/react";
 import App from "../App";
 
-test("Chakra Components tab is available", async () => {
-  render(<App />);
-  await waitFor(() => {
-    const linkElement = screen.getByText(/Chakra Components/i);
-    expect(linkElement).toBeInTheDocument();
-  });
-});
+function chackraComponentsTab(test: () => any): () => Promise<void> {
+  return async () => {
+    render(<App />);
+    await waitFor(() => {
+      const linkElement = screen.getByText(/Chakra Components/i);
+      expect(linkElement).toBeInTheDocument();
+      test()
+    });
+  }
+}
+test("Chakra Components tab is available", chackraComponentsTab(() => { }));
+
+test("Box renders", chackraComponentsTab(() => {
+  expect(screen.getByText("Menus")).toBeInTheDocument();
+}));
+
+test("Action Menu exists", chackraComponentsTab(() => {
+  expect(screen.getByText("Actions menu0001")).toBeInTheDocument();
+}));
