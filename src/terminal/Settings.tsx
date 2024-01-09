c5867b76b2cb6f1c02250427daa4025a5863933f
Kostas Kougios <kostas.kougios@googlemail.com>
2024-01-09 12:57:31 +0000

    -

--- src/terminal/Settings.tsx
+++ src/terminal/Settings.tsx
@@ -1,5 +1,6 @@
+import { ExternalLinkIcon } from "@chakra-ui/icons";
 import "./Terminal.css";
-import { Switch, useColorMode } from "@chakra-ui/react";
+import { Link, Switch, useColorMode } from "@chakra-ui/react";
 import { useToast } from "@chakra-ui/react";
 
 function Settings() {
@@ -11,6 +12,12 @@ function Settings() {
       <Switch onChange={toggleColorMode}>
         Toggle {colorMode === "light" ? "Dark" : "Light"} Mode
       </Switch>
+      <p style={ {'margin' : '25px'}}>
+        Have a question? Please ask at &nbsp;
+        <Link href='https://github.com/kostaskougios/terminal21-restapi/discussions' color='teal.500' isExternal>
+        terminal21's discussion board <ExternalLinkIcon mx='2px' />
+        </Link>
+      </p>
     </>
   );
 }
